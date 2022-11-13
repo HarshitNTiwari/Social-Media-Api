c5d7c79b2ba473961ea7c83d3e7d0b3e1c516613
Harshit Tiwari <harshitntiwari@gmail.com>
2022-11-13 15:40:22 +0530

    commit to main

--- api/database.py
+++ api/database.py
@@ -13,6 +13,7 @@ posts_collection = social_media_db.get_collection("posts")
 def follow_user(user_id, follow_id):
 	if user_id != follow_id:
 		user_data = users_collection.find_one({'_id': user_id}) 
+		print("user_data found")
 		followings = user_data['following']
 		
 		user_data = users_collection.find_one({'_id': follow_id})
@@ -26,19 +27,19 @@ def follow_user(user_id, follow_id):
 	return "Follow failed : follow_id same as user_id"
 
 def unfollow_user(user_id, unfollow_id):
-	if user_id != follow_id:
+	if user_id != unfollow_id:
 		user_data = users_collection.find_one({'_id': user_id}) 
 		followings = user_data['following']
 
-		if follow_id in followings:
-			users_collection.update_one({'_id': user_id}, {'$pull':{'following': follow_id}})
-			users_collection.update_one({'_id': follow_id}, {'$pull':{'follower': user_id}})
+		if unfollow_id in followings:
+			users_collection.update_one({'_id': user_id}, {'$pull':{'following': unfollow_id}})
+			users_collection.update_one({'_id': unfollow_id}, {'$pull':{'follower': user_id}})
 			return "OK"
-		return "Unfollow failed : follow_id not in following list of user_id"
-	return "Unfollow failed : follow_id same as user_id"
+		return "Unfollow failed : unfollow_id not in following list of user_id"
+	return "Unfollow failed : unfollow_id same as user_id"
 
-def get_user(email):
-	user_data = users_collection.find_one({'_id': email})
+def get_user(user_id):
+	user_data = users_collection.find_one({'_id': user_id})
 	if user_data:
 		return User(user_data['_id'], user_data['password'], user_data['name'], user_data['following'], user_data['follower'], user_data['liked_posts']) 
 	return None
@@ -46,33 +47,33 @@ def get_user(email):
 
 def add_post(post):
 	if post.title and post.desc:
-		posts_collection.insert_one({'_id': post.post_id, 'created_by': post.created_by, 'title': post.title, 'desc': post.desc, 'created_at': post.created_at})
-		return "OK"
-	else:
-		return "Title or Desc not found"
+		posts_collection.insert_one({'_id': post.post_id, 'created_by': post.created_by, 'title': post.title, 'desc': post.desc, 'created_at': post.created_at, 'comments': post.comments, 'likes_count': post.likes_count})
+		return post
+	return None
 
-def delete_post(post_id):
+def delete_post(post_id, user_id):
 	post = posts_collection.find_one({'_id': post_id})
-	if post:
+	if post and post['created_by'] == user_id:
 		posts_collection.delete_one({'_id': post_id})
 		return "OK"
 	return "Post not found"
 
 
-def like_post(user_id, post_id):
+def like_post(post_id, user_id):
 	post = posts_collection.find_one({'_id': post_id})
+	print(post)
 	if post:
 		user_data = users_collection.find_one({'_id': user_id})
 		liked_posts_list = user_data['liked_posts']
 		if post_id not in liked_posts_list:
-			users_collection.update_one({'_id': user_id}, {'$addToSet': { 'liked_posts': post_id }})
+			users_collection.update_one({'_id': user_id}, {'$addToSet': {'liked_posts': post_id }})
 			posts_collection.update_one({'_id': post_id}, {'$inc': {'likes_count': 1}})
 			return "OK"
 		else:
 			return "Post already in liked posts list of user_id"
 	return "Post ID not found"
 
-def unlike_post(user_id, id):
+def unlike_post(user_id, post_id):
 	post = posts_collection.find_one({'_id': post_id})
 	if post:
 		user_data = users_collection.find_one({'_id': user_id})
@@ -97,5 +98,5 @@ def add_comment(user_id, post_id, comment):
 def get_post(post_id):
 	post = posts_collection.find_one({'_id': post_id})
 	if post:
-		return Post(post['post_id'], post['created_by'], post['title'], post['desc'], post['created_at'], post['comment'], post['likes_count'])
-	return "Post ID not found"
+		return Post(post['_id'], post['created_by'], post['title'], post['desc'], post['created_at'], post['comments'], post['likes_count'])
+	return None
--- api/routes.py
+++ api/routes.py
@@ -16,7 +16,7 @@ def authenticate(email, password):
 
 def identity(payload):
     user_id = payload['identity']
-    print(user_id)
+    print("payload identity: ", user_id)
     return db.get_user(user_id)
 
 
@@ -26,14 +26,15 @@ jwt = JWT(app, authenticate, identity)
 @app.route('/api/follow/<string:id>', methods=['POST'])
 @jwt_required()
 def follow(id):
-	user_id = current_identity
+	user_id = current_identity.id
+	print("current identity: ",current_identity)
 	response = db.follow_user(user_id, id)
 	return response
 
 @app.route('/api/unfollow/<string:id>', methods=['POST'])
 @jwt_required()
 def unfollow(id):
-	user_id = current_identity
+	user_id = current_identity.id
 	response = db.unfollow_user(user_id, id)
 	return response
 
@@ -41,42 +42,46 @@ def unfollow(id):
 @app.route('/api/user', methods=['GET'])
 @jwt_required()
 def getUser():
-	user_id = current_identity
+	user_id = current_identity.id
 	user = db.get_user(user_id)
 	if user:
 		response = {'name': user.name, 'No. of followers': len(user.follower), 'No. of followings': len(user.following)}
 		return jsonify(response)
 	return jsonify({"Message": "User not found"})
 
-@app.route('/api/posts/', methods=['POST'])
+@app.route('/api/posts', methods=['POST'])
 @jwt_required()
 def addPost():
 	content_type = request.headers.get('Content-Type')
 	if (content_type == 'application/json'):
 		post_data = request.json
-	user_id = current_identity
-	post = Post(str(uuid.uuid1()), post_data['title'], post_data['desc'], user_id, str(datetime.now()))
-	response = db.add_post(post)
-	return response, 200 
+	user_id = current_identity.id
+	post = Post(str(uuid.uuid1()), user_id, post_data['title'], post_data['desc'], str(datetime.now()), [], 0)
+	post = db.add_post(post)
+	if post:
+		response = {'Post-ID': post.post_id, 'Title': post.title, 'Description': post.desc, 'Created Time': post.created_at}
+		return jsonify(response)
+	return jsonify({"Message": "Title or Desc not found"}) 
 
 @app.route('/api/posts/<string:id>', methods=['DELETE'])
 @jwt_required()
 def deletePost(id):
-	user_id = current_identity
-	response = db.delete_post(id)
+	user_id = current_identity.id
+	response = db.delete_post(id, user_id)
 	return response 
 
 @app.route('/api/like/<string:id>', methods=['POST'])
 @jwt_required()
 def likePost(id):
-	user_id = current_identity
-	response = db.like_post(user_id, id)
+	user_id = current_identity.id
+	print(id)
+	response = db.like_post(id, user_id)
 	return response
 
 @app.route('/api/unlike/<string:id>', methods=['POST'])
 @jwt_required()
 def unlikePost(id):
-	user_id = current_identity
+	user_id = current_identity.id
 	response = db.unlike_post(user_id, id)
 	return response
 
@@ -87,7 +92,7 @@ def postComment(id):
 	if (content_type == 'application/json'):
 		comment_data = request.json
 	comment = comment_data['comment']
-	user_id = current_identity
+	user_id = current_identity.id
 	response = db.add_comment(user_id, id, comment)
 	return response 
 
@@ -103,5 +108,5 @@ def getPost(id):
 # @app.route('/api/all_posts', methods=['GET'])
 # @jwt_required()
 # def get():
-# 	user_id = current_identity
+# 	user_id = current_identity.id
 # 	get_all_posts(user_id)
